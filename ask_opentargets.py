4190e9374c917a7ab4cd750e6d87924a5de86b41
Kelvin Liu <e.cesare@gmail.com>
2023-07-03 07:30:59 -0400

    Implement solution to coding challenge

--- ask_opentargets.py
+++ ask_opentargets.py
@@ -1,66 +1,80 @@
+import datetime
 import os
 import re
 import json
+from typing import Mapping
+
 import requests
 import openai
-from datetime import datetime
-from utils import extract_values
 
-# read Open AI API key from environment variable
+import utils
+
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-# Open Targets graphQL schema example
-# read from file
-with open("graphql_schema.txt", "r") as f:
-    prompt_template = f.read()
 
-# Prime the target query for completion
-prime_prompt = "query top_n_associated_diseases {\n  search(queryString:"
+def main():
+    # Custom input by the user
+    # user_input = "Find the top 2 diseases associated with BRCA1"
+    user_input = input("How can I help you today?\n")
 
-# Custom input by the user
-# user_input = "Find the top 2 diseases associated with BRCA1"
-user_input = input("How can I help you today?\n")
+    print("\nGenerating Open Targets GraphQL query with GPT...")
 
-response = openai.Completion.create(
-    model="code-davinci-002",
-    prompt=prompt_template + "### " + user_input + "\n" + prime_prompt,
-    temperature=0,
-    max_tokens=250,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0,
-    stop=["###"],
-)
-response_text = response["choices"][0].text
+    # TODO: Current prompt is low accuracy because we only provide an example query for one use
+    # case. Rather than using a single prompt to generate the GraphQL query, we could improve both
+    # efficiency/cost and accuracy by first classifying the use case, and including only that
+    # example in the prompt.
+    # Another alternative, which at least improves accuracy, is simply providing an example of
+    # every query type (assuming they can all fit within a single prompt).
+    # TODO: Should gracefully handle persistent OpenAI errors, or failed retry of transient errors
+    query_string = utils.call_gpt(
+        template="graphql_schema.txt",
+        query=user_input,
+        primer="query ",
+    )
 
-query_string = prime_prompt + response_text
+    # filename with current date and time
+    query_file = "query_" + datetime.datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + ".txt"
 
-# filename with current date and time
-query_file = "query_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + ".txt"
+    # write query to file with current date and time
+    with open(query_file, "w") as f:
+        f.write(f"# User input: {user_input}\n")
+        f.write(query_string)
+        print(f"\nCustom GraphQL query was written to file: {query_file}")
 
-# write query to file with current date and time
-with open(query_file, "w") as f:
-    f.write(f"# User input: {user_input}\n")
-    f.write(query_string)
-    print(f"\nCustom graphQL query was written to file: {query_file}")
+    print("\nQuerying Open Targets genetics database...")
 
-# Set base URL of GraphQL API endpoint
-base_url = "https://api.platform.opentargets.org/api/v4/graphql"
+    # Set base URL of GraphQL API endpoint
+    base_url = "https://api.platform.opentargets.org/api/v4/graphql"
 
-# Perform POST request and check status code of response
-# This handles the cases where the Open Targets API is down or our query is invalid
-try:
-    response = requests.post(base_url, json={"query": query_string})
-    response.raise_for_status()
-except requests.exceptions.HTTPError as err:
-    print(err)
+    # Perform POST request and check status code of response
+    # This handles the cases where the Open Targets API is down or our query is invalid
+    try:
+        response = requests.post(base_url, json={"query": query_string})
+        api_response = json.loads(response.text)
+        response.raise_for_status()
+    except Exception as ex:
+        print(api_response)
+        raise ex
 
-# Transform API response from JSON into Python dictionary and print in console
-api_response = json.loads(response.text)
-hits_list = api_response["data"]["search"]["hits"][0]
+    # Transform API response from JSON into Python dictionary and print in console
+    api_response = json.loads(response.text)
+    if not isinstance(api_response, Mapping) or not api_response.get("data", {}).get("search", {}).get("hits", []):
+        raise ValueError('no information found in Open Targets API output')
+    hits_list = api_response["data"]["search"]["hits"][0]
 
-print("\n\nQuerying Open Targets genetics database...\n\n")
+    # TODO: Could obviously be more efficient here
+    disease_list = utils.extract_values(hits_list, "disease")
+    drug_list = utils.extract_values(hits_list, "drug")
+    if disease_list or drug_list:
+        for i, j in enumerate(disease_list or drug_list):
+            print(f"{i+1}. {j['name']}")
+    else:
+        target_list = utils.extract_values(hits_list, "target")
+        # TODO: Not sure why querying targets for a drug uses a completely different syntax
+        assoc_target_list = utils.extract_values(hits_list, "rows")
+        for i, j in enumerate(target_list or assoc_target_list):
+            print(f"{i+1}. {j['approvedName']}")
 
-disease_list = extract_values(hits_list, "disease")
-for i, j in enumerate(disease_list):
-    print(f"{i+1}. {j['name']}")
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,4 +1,45 @@
+import functools as ft
 from typing import Dict, List, Union, Any
+
+import openai
+# import tenacity
+
+
+# TODO: Should just use a prompting package like LangChain or Guidance
+# TODO: Should use vector cache to retrieve semantically similar inputs
+@ft.cache
+# @tenacity.retry(
+#     wait=tenacity.wait_random_exponential(multiplier=0.3, exp_base=3, max=90),
+#     stop=tenacity.stop_after_attempt(7),
+#     retry=tenacity.retry_if_exception_type(
+#         (openai.error.APIError, openai.error.TryAgain, openai.error.Timeout,
+#          openai.error.APIConnectionError, openai.error.RateLimitError,
+#          openai.error.ServiceUnavailableError, )),
+#     reraise=True,
+# )
+def call_gpt(template: str, model: str = "gpt-3.5-turbo-16k", **kwargs) -> str:
+    # Open Targets graphQL schema example
+    # read from file
+    with open(template, "r") as f:
+        prompt_template = f.read()
+
+    # TODO: Need to call different class for Completion models
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=[{'role': 'user', "content": prompt_template.format(**kwargs)}],
+        # TODO: Should parameterize GPT parameters. Most prompting packages separate the AI model
+        # initialization from the prompt templating/calling.
+        temperature=0,
+        max_tokens=250,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=["###"],
+    )
+    response_text = response["choices"][0]['message']['content']
+    primer = kwargs.get('primer', '')
+    return primer + response_text
+
 
 # This function is used to extract values from a (deep) nested JSON object
 # Credit: https://gist.github.com/toddbirchard/b6f86f03f6cf4fc9492ad4349ee7ff8b?permalink_comment_id=4178846#gistcomment-4178846
